2b7fcfb099a8d4ef00f1f8dd9a423969d526a25f
JiLIN <zeikling@outlook.com>
2026-05-25 04:33:58 +1000

    Add instrumented UI tests for main screen

--- app/src/androidTest/java/com/example/stemlab/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/stemlab/ExampleInstrumentedTest.kt
@@ -1,24 +1,36 @@
 package com.example.stemlab
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.stemlab", appContext.packageName)
+    fun mainScreen_displaysAppTitle() {
+        onView(withText("STEMM Lab"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun mainScreen_displaysSubtitle() {
+        onView(withText("Real-World STEMM Games"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun mainScreen_displaysStartButton() {
+        onView(withText("Start"))
+            .check(matches(isDisplayed()))
     }
 }
